06ce8a758486ece819273a66b615c44bd1290210
shhong <valentine15year@gmail.com>
2022-11-17 23:22:23 +0900

    build: sass 설정 및 기본 스타일 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 import eslintPlugin from 'vite-plugin-eslint';
 
 export default defineNuxtConfig({
+    css: ['~/assets/css/base.scss'],
     vite: {
         plugins: [
             eslintPlugin({
