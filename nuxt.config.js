779b77d5127084449e3f1e4e96e6001f2b717727
shhong <valentine15year@gmail.com>
2022-11-25 23:57:21 +0900

    fix: nuxt3 styleResources 전역 sass import 이슈 해결

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,11 +2,19 @@ import eslintPlugin from 'vite-plugin-eslint';
 
 export default defineNuxtConfig({
     css: ['~/assets/css/base.scss'],
-    styleResources: {
-        scss: '~/assets/css/*.scss',
-    },
     buildModules: ['nuxt-vite', '@nuxtjs/style-resources'],
     vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    sourceMap: true,
+                    additionalData: `
+                        @import "@/assets/css/common/variables.scss";
+                        @import "@/assets/css/common/mixins.scss"; 
+                    `,
+                },
+            },
+        },
         plugins: [
             eslintPlugin({
                 failOnError: false,
