9f0444a239ca233b5fe2937425515513faea96a8
shhong <valentine15year@gmail.com>
2022-11-25 23:17:51 +0900

    build: style-resources 설치

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,10 @@ import eslintPlugin from 'vite-plugin-eslint';
 
 export default defineNuxtConfig({
     css: ['~/assets/css/base.scss'],
+    styleResources: {
+        scss: '~/assets/css/*.scss',
+    },
+    buildModules: ['nuxt-vite', '@nuxtjs/style-resources'],
     vite: {
         plugins: [
             eslintPlugin({
