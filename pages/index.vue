e9588a44c60897a486051aa5242414da22fd3783
shhong <valentine15year@gmail.com>
2022-11-27 01:46:53 +0900

    feat: spoqa 폰트 및 텍스트 관련 css mixin 추가

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,27 @@
 <template>
-    <h1>Hello, I'm an Markup Developer</h1>
-    <p>blah blah blah blah blah blah blah blah blah blah blah</p>
+    <h1>Hello, I'm an UI/UX Engineer</h1>
+    <h2>Hello, I'm an UI/UX Engineer</h2>
+    <h3>Hello, I'm an UI/UX Engineer</h3>
+    <p>
+        bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra
+        <br />
+        bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra
+        <br />
+        적을 내용이 많았으면 좋겠다. 적을 내용이 많았으면 좋겠다. 적을 내용이
+        많았으면 좋겠다. <br />
+        적을 내용이 많았으면 좋겠다. 적을 내용이 많았으면 좋겠다. 적을 내용이
+        많았으면 좋겠다. <br />
+    </p>
+    <p class="font-cap">
+        bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra
+        <br />
+        bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra
+        <br />
+        적을 내용이 많았으면 좋겠다. 적을 내용이 많았으면 좋겠다. 적을 내용이
+        많았으면 좋겠다. <br />
+        적을 내용이 많았으면 좋겠다. 적을 내용이 많았으면 좋겠다. 적을 내용이
+        많았으면 좋겠다. <br />
+    </p>
     <button>Contact me</button>
 </template>
 
