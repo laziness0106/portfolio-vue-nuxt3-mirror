f2a0acfc41aa852ff10436722fbe64b450be12aa
shhong <valentine15year@gmail.com>
2022-11-26 18:57:42 +0900

    feat: main layout 추가

---
+++ layouts/main.vue
@@ -0,0 +1,19 @@
+<template>
+    <main class="layout-main">
+        <slot />
+    </main>
+</template>
+
+<style lang="scss">
+html,
+body,
+#__nuxt {
+    width: 100%;
+    height: 100%;
+}
+.layout-main {
+    width: 100%;
+    height: 100%;
+    padding: 4rem;
+}
+</style>
