2e2091b0fa8fefca1192f9869214a8bb8eca86b4
shhong <valentine15year@gmail.com>
2022-11-27 16:22:43 +0900

    feat: index 페이지 마크업

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,19 @@
 <template>
-    <h1>Hello, I'm an UI/UX Engineer</h1>
-    <h2>Hello, I'm an UI/UX Engineer</h2>
-    <h3>Hello, I'm an UI/UX Engineer</h3>
-    <p>
-        bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra
-        <br />
-        bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra
-        <br />
-        적을 내용이 많았으면 좋겠다. 적을 내용이 많았으면 좋겠다. 적을 내용이
-        많았으면 좋겠다. <br />
-        적을 내용이 많았으면 좋겠다. 적을 내용이 많았으면 좋겠다. 적을 내용이
-        많았으면 좋겠다. <br />
-    </p>
-    <p class="font-cap">
-        bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra
-        <br />
-        bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra
-        <br />
-        적을 내용이 많았으면 좋겠다. 적을 내용이 많았으면 좋겠다. 적을 내용이
-        많았으면 좋겠다. <br />
-        적을 내용이 많았으면 좋겠다. 적을 내용이 많았으면 좋겠다. 적을 내용이
-        많았으면 좋겠다. <br />
-    </p>
+    <section>
+        <h1>Hello, I'm an UI/UX Engineer</h1>
+        <p>
+            bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra
+            bra
+            <br />
+            bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra
+            bra
+            <br />
+            적을 내용이 많았으면 좋겠다. 적을 내용이 많았으면 좋겠다. 적을
+            내용이 많았으면 좋겠다. <br />
+            적을 내용이 많았으면 좋겠다. 적을 내용이 많았으면 좋겠다. 적을
+            내용이 많았으면 좋겠다. <br />
+        </p>
+    </section>
     <button>Contact me</button>
 </template>
 
@@ -30,3 +22,20 @@ definePageMeta({
     layout: 'main',
 });
 </script>
+
+<style lang="scss">
+.layout-main {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    row-gap: 2.8rem;
+    h1 {
+        + p {
+            margin-top: 2rem;
+        }
+    }
+    button {
+        width: 10rem;
+    }
+}
+</style>
