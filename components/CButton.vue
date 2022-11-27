b7d0b367a19dd79dccf507445ce11c72d35f5e55
shhong <valentine15year@gmail.com>
2022-11-27 19:54:56 +0900

    feat: CButton 컴포넌트 추가

---
+++ components/CButton.vue
@@ -0,0 +1,67 @@
+<template>
+    <button :class="[typeClass, sizeClass]">
+        {{ text }}
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'CButton',
+    props: [
+        /* button의 내용 */
+        'text',
+        /* button의 타입
+         * 'primary'
+         * 'secondary'
+         * 'ghost'
+         */
+        'type',
+        /* button의 크기
+         * 'sm'
+         * 'md'
+         * 'lg'
+         */
+        'size',
+    ],
+    data() {
+        return {
+            typeClass: this.type,
+            sizeClass: this.size,
+        };
+    },
+};
+</script>
+
+<style lang="scss">
+button {
+    white-space: nowrap;
+    &.primary {
+        background-color: var(--primary-color);
+        color: white;
+    }
+    &.secondary {
+        background-color: var(--secondary-color);
+        color: white;
+    }
+    &.ghost {
+        background-color: transparent;
+        color: var(--primary-color);
+        border: 1px solid var(--primary-color);
+    }
+    &.sm {
+        @include font-cap();
+        padding: 0.4rem 0.8rem;
+        border-radius: 0.4rem;
+    }
+    &.md {
+        @include font-content();
+        padding: 0.8rem 2rem;
+        border-radius: 0.4rem;
+    }
+    &.lg {
+        @include font-h3();
+        padding: 1.6rem 2.4rem;
+        border-radius: 0.4rem;
+    }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,22 @@
 <template>
-    <section>
-        <h1>Hello, I'm an UI/UX Engineer</h1>
-        <p>
-            bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra
-            bra
-            <br />
-            bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra
-            bra
-            <br />
-            적을 내용이 많았으면 좋겠다. 적을 내용이 많았으면 좋겠다. 적을
-            내용이 많았으면 좋겠다. <br />
-            적을 내용이 많았으면 좋겠다. 적을 내용이 많았으면 좋겠다. 적을
-            내용이 많았으면 좋겠다. <br />
-        </p>
-        <button>Contact me</button>
-    </section>
+    <div class="page-container">
+        <section>
+            <h1>Hello, I'm an UI/UX Engineer</h1>
+            <p>
+                bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra
+                bra bra
+                <br />
+                bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra bra
+                bra bra
+                <br />
+                적을 내용이 많았으면 좋겠다. 적을 내용이 많았으면 좋겠다. 적을
+                내용이 많았으면 좋겠다. <br />
+                적을 내용이 많았으면 좋겠다. 적을 내용이 많았으면 좋겠다. 적을
+                내용이 많았으면 좋겠다. <br />
+            </p>
+        </section>
+        <CButton text="Contact me" type="primary" size="md" />
+    </div>
 </template>
 
 <script>
@@ -25,17 +27,22 @@ definePageMeta({
 
 <style lang="scss">
 .layout-main {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    row-gap: 2.8rem;
-    h1 {
-        + p {
-            margin-top: 2rem;
+    .page-container {
+        width: 100%;
+        height: 100%;
+        section {
+            width: 100%;
+            height: 100%;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            row-gap: 2.8rem;
+            h1 {
+                + p {
+                    margin-top: 2rem;
+                }
+            }
         }
-    }
-    button {
-        width: 10rem;
     }
 }
 </style>
