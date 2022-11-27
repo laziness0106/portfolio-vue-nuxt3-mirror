131c2710d83c36e751991cb102dee203959a218b
shhong <valentine15year@gmail.com>
2022-11-28 02:13:03 +0900

    feat: works layouts 추가

---
+++ layouts/works.vue
@@ -0,0 +1,25 @@
+<template>
+    <main class="layout-works">
+        <NuxtLink to="/works" class="btn-close">닫기</NuxtLink>
+        <slot />
+    </main>
+</template>
+
+<style lang="scss">
+html,
+body,
+#__nuxt {
+    width: 100%;
+    height: 100%;
+}
+.layout-works {
+    width: 100%;
+    height: 100%;
+    .btn-close {
+        position: absolute;
+        top: 0;
+        right: 0;
+        padding: 2rem;
+    }
+}
+</style>
