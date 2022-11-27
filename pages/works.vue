b8cff2f3b598160889d1f1b5b13a20cb4d1db7b1
shhong <valentine15year@gmail.com>
2022-11-27 22:03:24 +0900

    feat: works 페이지 마크업 및 임시 style 추가

--- pages/works.vue
+++ pages/works.vue
@@ -1,7 +1,16 @@
 <template>
     <div class="page-container">
-        <h1>WORKS</h1>
-        <p>blah blah blah blah blah blah blah blah blah blah blah</p>
+        <section>
+            <h1 class="ally-hidden">WORKS</h1>
+            <ul class="work-list-container">
+                <li class="work-item">work-item</li>
+                <li class="work-item">work-item</li>
+                <li class="work-item">work-item</li>
+                <li class="work-item">work-item</li>
+                <li class="work-item">work-item</li>
+                <li class="work-item">work-item</li>
+            </ul>
+        </section>
     </div>
 </template>
 
@@ -10,3 +19,43 @@ definePageMeta({
     layout: 'main',
 });
 </script>
+
+<style lang="scss">
+.layout-main {
+    .page-container {
+        width: 100%;
+        height: 100%;
+        section {
+            width: 180%;
+            height: 80%;
+            margin: 4rem 0;
+            .work-list-container {
+                display: grid;
+                // grid-template-columns: 1fr repeat(auto-fit, minmax(40rem, 1fr));
+                grid-template-columns: 50% 1fr 1fr;
+                gap: 0.4rem;
+                grid-auto-flow: dense;
+                align-items: stretch;
+                .work-item {
+                    background: #888888;
+                    text-align: center;
+                    border-radius: 0.8rem;
+                    height: 100%;
+                    min-height: 16rem;
+                    display: flex;
+                }
+                .work-item:nth-child(1) {
+                    grid-column: 1 / span 1;
+                    grid-row: 1 / span 2;
+                }
+                .work-item:nth-child(2) {
+                    grid-column: 2 / span 2;
+                }
+                .work-item:nth-child(6) {
+                    grid-column: 2 / span 2;
+                }
+            }
+        }
+    }
+}
+</style>
