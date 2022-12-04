f21534546922935d68cfaea183cc6de85665f3db
shhong <valentine15year@gmail.com>
2022-12-04 15:23:29 +0900

    fix: Header 컴포넌트 겹침 해결 및 background 추가

--- components/Header.vue
+++ components/Header.vue
@@ -8,11 +8,13 @@
 <style lang="scss">
 header {
     position: fixed;
+    z-index: 1000;
     top: 0;
     left: 0;
     display: flex;
     justify-content: space-around;
     width: 100%;
     padding: 2rem 4rem;
+    bacgkround: rgba(255, 255, 255, 0.4);
 }
 </style>
