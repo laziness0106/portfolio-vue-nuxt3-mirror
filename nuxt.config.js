8d02b519a6343b41e4cae6a08011cfb9b110af15
shhong <valentine15year@gmail.com>
2022-12-08 03:38:53 +0900

    fix: nuxt3 metatag 미적용 이슈 해결

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,44 +1,6 @@
 import eslintPlugin from 'vite-plugin-eslint';
 
 export default defineNuxtConfig({
-    head: {
-        title: 'portfolio-vue-nuxt3',
-        meta: [
-            { charset: 'utf-8' },
-            {
-                name: 'viewport',
-                content: 'width=device-width, initial-scale=1',
-            },
-            {
-                hid: 'description',
-                name: 'og:description',
-                content: 'shhong portfolio',
-            },
-            {
-                name: 'og:title',
-                content: 'portfolio-vue-nuxt3',
-            },
-            {
-                name: 'og:keywords',
-                content: 'portfolio, vue, nuxt',
-            },
-            {
-                name: 'og:image',
-                content: '@/assets/images/thumbnail.png',
-            },
-            {
-                name: 'og:author',
-                content: 'shhong',
-            },
-        ],
-        link: [
-            {
-                rel: 'icon',
-                type: 'image/png',
-                href: '@/assets/images/favicon.png',
-            },
-        ],
-    },
     target: 'server',
     ssr: true,
     router: {
@@ -46,6 +8,44 @@ export default defineNuxtConfig({
     },
     css: ['~/assets/css/base.scss'],
     app: {
+        head: {
+            title: 'portfolio-vue-nuxt3',
+            meta: [
+                { charset: 'utf-8' },
+                {
+                    name: 'viewport',
+                    content: 'width=device-width, initial-scale=1',
+                },
+                {
+                    hid: 'description',
+                    name: 'og:description',
+                    content: 'shhong portfolio',
+                },
+                {
+                    name: 'og:title',
+                    content: 'portfolio-vue-nuxt3',
+                },
+                {
+                    name: 'og:keywords',
+                    content: 'portfolio, vue, nuxt',
+                },
+                {
+                    name: 'og:image',
+                    content: '@/assets/images/thumbnail.png',
+                },
+                {
+                    name: 'og:author',
+                    content: 'shhong',
+                },
+            ],
+            link: [
+                {
+                    rel: 'icon',
+                    type: 'image/png',
+                    href: '@/assets/images/favicon.png',
+                },
+            ],
+        },
         pageTransition: { name: 'page', mode: 'out-in' },
     },
     buildModules: ['nuxt-vite', '@nuxtjs/style-resources'],
