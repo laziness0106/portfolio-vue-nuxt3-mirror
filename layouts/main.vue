e4471196e86788f83527b171556ead9f8ec3844e
SMILEGATE\soonhyunhong <soonhyunhong@smilegate.com>
2022-12-20 18:11:12 +0900

    feat: main 페이지 신규 작성

--- layouts/main.vue
+++ layouts/main.vue
@@ -1,5 +1,4 @@
 <template>
-    <Header />
     <main class="layout-main">
         <slot />
     </main>
@@ -11,22 +10,5 @@ body,
 #__nuxt {
     width: 100%;
     height: 100%;
-}
-.layout-main {
-    width: 100%;
-    height: 100%;
-    background: url('@/assets/images/bg-main.jpg');
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: 0 80%;
-    .page-container {
-        padding: 8.4rem 4rem 4rem 4rem;
-        @include tablet {
-            padding: 2rem;
-        }
-        @include mobile {
-            padding: 1.6rem;
-        }
-    }
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,26 @@
 <template>
     <div class="page-container">
-        <section>
-            <h2>홍순현</h2>
-            <ul>
-                <li>
-                    <CLabel type="primary" text="html" size="md" />
-                </li>
-                <li>
-                    <CLabel type="secondary" text="sass" size="md" />
-                </li>
-                <li>
-                    <CLabel type="tertiary" text="JavaScript" size="md" />
-                </li>
-                <li>
-                    <CLabel type="pink" text="Vue" size="md" />
-                </li>
-                <li>
-                    <CLabel type="pink-light" text="Nuxt" size="md" />
-                </li>
-                <li>
-                    <CLabel type="yellow" text="Typescript" size="md" />
-                </li>
-                <li>
-                    <CLabel type="blue" text="Github" size="md" />
-                </li>
-                <li>
-                    <CLabel type="blue-light" text="Tailwind" size="md" />
-                </li>
-                <li>
-                    <CLabel type="blue-dark" text="bootstrap" size="md" />
-                </li>
+        <nav>
+            <ul class="gnb">
+                <li data-menuanchor="section01"><a href="#section01">section01</a></li>
+                <li data-menuanchor="section02"><a href="#section02">section02</a></li>
+                <li data-menuanchor="section03"><a href="#section03">section03</a></li>
             </ul>
-        </section>
+        </nav>
+        <full-page :options="options">
+            <section class="section main-section">
+                <figure class="logo-main">
+                    <img src="@/assets/images/logo-main.png" alt="hong soon hyun" />
+                    <figcaption>2019-2022 홍순현</figcaption>
+                </figure>
+            </section>
+            <section class="section">
+                section02
+            </section>
+            <section class="section">
+                section03
+            </section>
+        </full-page>
     </div>
 </template>
 
@@ -39,43 +28,66 @@
 definePageMeta({
     layout: 'main',
 });
+export default {
+    data() {
+        return {
+            options: {
+                licenseKey: 'YOUR_KEY_HERE',
+                scrollBar: false,
+                menu: '.gnb',
+                navigation: true,
+                anchors: [
+                    'section01',
+                    'section02',
+                    'section03',
+                ],
+            },
+        }
+    }
+}
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .layout-main {
-    .page-container {
-        width: 100%;
-        height: 100%;
-        display: flex;
-        align-items: center;
-        column-gap: 4rem;
-        section {
+    nav {
+        position: absolute;
+        z-index: 10;
+        top: 0;
+    }
+    .fullpage-wrapper {
+        > section {
             width: 100%;
-            text-align: center;
-            > h2 {
-                font-size: 20rem;
-                line-height: 24rem;
-                @include tablet {
-                    font-size: 12rem;
-                    line-height: 16rem;
-                }
-                @include mobile {
-                    font-size: 6rem;
-                    line-height: 8rem;
+            height: 100vh;
+            .fp-overflow {
+                width: 100%;
+                height: 100vh;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                font-size: 25px;
+            }
+            &.main-section {
+                background-color: var(--on-background-color-3);
+                display: flex;
+                align-items: center;
+                .logo-main {
+                    width: 34%;
+                    position: relative;
+                    img {
+                        width: 100%;
+                    }
+                    figcaption {
+                        position: absolute;
+                        right: 0;
+                        bottom: 27%;
+                        font-size: 1vw;
+                        font-weight: 100;
+                        color: #ffffff;
+                    }
                 }
             }
-            > ul {
-                display: flex;
-                flex-wrap: wrap;
-                justify-content: center;
-                margin-top: 4rem;
-                gap: 1.2rem;
-                @include tablet {
-                    margin-top: 2.8rem;
-                }
-                @include mobile {
-                    margin-top: 1.2rem;
-                }
+            .fp-watermark {
+                display: none;
             }
         }
     }
